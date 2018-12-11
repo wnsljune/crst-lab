b46a94a0d9b7ead0b11029a313dd53939855c80c
JunHee Han <wnsljune@gmail.com>
2018-12-11 10:33:28 +0900

    테스트입니다.

---
+++ crst/src/main/java/com/crustec/CrstApplication.java
@@ -0,0 +1,12 @@
+package com.crustec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrstApplication.class, args);
+	}
+}
